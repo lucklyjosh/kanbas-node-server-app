569ee5336c4d831f90cadd5f33da51c1665e414d
lucklyjosh <lucklyjosh@gmail.com>
2023-11-18 19:51:42 -0500

    a5

---
+++ app.js
@@ -0,0 +1,20 @@
+// const express = require("express");
+import express from "express";
+import Hello from "./hello.js";
+import Lab5 from "./lab5.js";
+import CourseRoutes from "./courses/routes.js";
+import cors from "cors";
+import ModuleRoutes from "./modules/routes.js";
+import "dotenv/config";
+
+const app = express();
+
+app.use(cors());
+CourseRoutes(app);
+app.use(express.json());
+ModuleRoutes(app);
+
+Lab5(app);
+Hello(app);
+
+app.listen(process.env.PORT || 4000);
