a96cdb823da8700f659a986305f67253b83ceb1f
lucklyjosh <lucklyjosh@gmail.com>
2023-12-02 08:11:00 -0500

    a6 Mongo fa23

--- app.js
+++ app.js
@@ -6,12 +6,40 @@ import CourseRoutes from "./courses/routes.js";
 import cors from "cors";
 import ModuleRoutes from "./modules/routes.js";
 import "dotenv/config";
+import mongoose from "mongoose";
+import UserRoutes from "./users/routes.js";
+mongoose.connect("mongodb://127.0.0.1:27017/kanbas");
+import session from "express-session";
+import "dotenv/config";
 
 const app = express();
+const CONNECTION_STRING = process.env.DB_CONNECTION_STRING || 'mongodb://127.0.0.1:27017/kanbas'
+mongoose.connect(CONNECTION_STRING);
 
-app.use(cors());
+app.use(cors({
+    credentials: true,
+    origin: process.env.FRONTEND_URL
+}));
+
 CourseRoutes(app);
+
+const sessionOptions = {
+    secret: "any string",
+    resave: false,
+    saveUninitialized: false,
+};
+if (process.env.NODE_ENV !== "development") {
+    sessionOptions.proxy = true;
+    sessionOptions.cookie = {
+        sameSite: "none",
+        secure: true,
+    };
+}
+app.use(session(sessionOptions));
+
+
 app.use(express.json());
+UserRoutes(app);
 ModuleRoutes(app);
 
 Lab5(app);
